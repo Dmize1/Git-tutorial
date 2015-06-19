5a34940102499e4a2b669792c48ece6839f3a1d3
Dylan Franco <dfranco808@gmail.com>
2015-06-19 13:57:46 -1000

    Merged master fixed conflict

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,11 +1,7 @@
 require 'greeter'
 
-# Default is World
-# Author: Dylan Franco (dfranco808@gmail.com)
-name = ARGV.first || "World"
-
-greeter = Greeter.new(name)
-puts "What's your name?"
+puts "What's your name"
 my_name = gets.strip
 
-puts "Hello, #{my_name} !"
+greeter = Greeter.new(my_name)
+puts greeter.greet
